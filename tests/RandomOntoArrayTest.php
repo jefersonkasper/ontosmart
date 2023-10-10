75c7ec33a4c7cdb504d03bb027af97b7c4b897ba
kasper <jefersonkasper@gmail.com>
2023-10-09 22:44:43 -0300

    teste

--- tests/RandomOntoArrayTest.php
+++ tests/RandomOntoArrayTest.php
@@ -1,5 +1,5 @@
 <?php
-use Jefersonkasper\Ontosmart\OntoSmart;
+use Jefersonkasper\Ontosmart\Ontosmart;
 it('Should return an array with keys and values, using color value', function(){
 	$fakeClient = new OntoSmart();
 	$arr = $fakeClient->getArrayValues('color');
